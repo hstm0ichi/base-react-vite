71b5b41a11a07a7a1535cabcd99f17c9fd60c2e4
hstm0ichi <stngocquynew@gmail.com>
2024-07-23 12:32:34 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,26 @@
+
 const TodoNew = (props) => {
     const { addNewToDo } = props;
-    addNewToDo("STM")
+
+    const handleClick = () => {
+        alert("Click me");
+    }
+
+    const handleOnChange = (name) => {
+
+        alert("Change: " + name);
+    }
+
     return (
         <>
             <div className='todo-new'>
-                <input type="text" />
-                <button>Add</button>
+                <input type="text"
+                    onChange={() => { handleOnChange(event.target.value) }}
+                />
+                <button
+                    onClick={handleClick}
+                    style={{ cursor: "pointer" }}
+                >Add</button>
             </div>
         </>
     )
