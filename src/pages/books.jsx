0631535cef7a3697bf328043d32d1b4e7ab58f63
hstm0ichi <stngocquynew@gmail.com>
2024-07-23 16:21:56 +0700

    video 52

---
+++ src/pages/books.jsx
@@ -0,0 +1,10 @@
+
+const BooksPage = () => {
+    return (
+        <h1>
+            Books Page
+        </h1>
+    )
+}
+
+export default BooksPage;
---
+++ src/pages/error.jsx
@@ -0,0 +1,17 @@
+import { useRouteError, Link } from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    return (
+        <div id="error-page" style={{ textAlign: "center" }}>
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+            <Link to="/">
+                <span>Back to Homepage</span>
+            </Link>
+        </div>
+    );
+}
